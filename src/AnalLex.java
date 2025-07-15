108df3f3091192d69ab1c0869209dbf3e0f54fb9
bobelbo <brij0701@usherbrooke.ca>
2025-07-15 16:28:15 -0400

    Bro git a pas track mes chits (code 18)

---
+++ src/AnalLex.java
@@ -0,0 +1,70 @@
+package app6.src;
+
+import app6.src.filelib.Reader;
+import app6.src.filelib.Writer;
+
+/** Cette classe effectue l'analyse lexicale
+ */
+public class AnalLex {
+
+// Attributs
+  private final String formule;
+
+
+/** Constructeur pour l'initialisation d'attribut(s)
+ */
+  public AnalLex(String forumle) {  // arguments possibles
+    this.formule = forumle;  // Initialisation de l'attribut formule
+  }
+
+
+/** resteTerminal() retourne :
+      false  si tous les terminaux de l'expression arithmetique ont ete retournes
+      true s'il reste encore au moins un terminal qui n'a pas ete retourne 
+ */
+  public boolean resteTerminal( ) {
+    //
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+  
+  
+/** prochainTerminal() retourne le prochain terminal
+      Cette methode est une implementation d'un AEF
+ */  
+  public Terminal prochainTerminal( ) {
+     //
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+ 
+/** ErreurLex() envoie un message d'erreur lexicale
+ */ 
+  public void ErreurLex(String s) {	
+     //
+  }
+
+  
+  //Methode principale a lancer pour tester l'analyseur lexical
+  public static void main(String[] args) {
+    String toWrite = "";
+    System.out.println("Debut d'analyse lexicale");
+    if (args.length == 0){
+    args = new String [2];
+            args[0] = "ExpArith.txt";
+            args[1] = "ResultatLexical.txt";
+    }
+    Reader r = new Reader(args[0]);
+
+    AnalLex lexical = new AnalLex(r.toString()); // Creation de l'analyseur lexical
+
+    // Execution de l'analyseur lexical
+    Terminal t = null;
+    while(lexical.resteTerminal()){
+      t = lexical.prochainTerminal();
+      toWrite += t.chaine + "\n" ;  // toWrite contient le resultat
+    }				   //    d'analyse lexicale
+    System.out.println(toWrite); 	// Ecriture de toWrite sur la console
+    Writer w = new Writer(args[1],toWrite); // Ecriture de toWrite dans fichier args[1]
+    System.out.println("Fin d'analyse lexicale");
+  }
+}
