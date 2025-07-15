cdfb10aa3ba44fa242a62973bea01c3899466a7b
bobelbo <brij0701@usherbrooke.ca>
2025-07-15 16:12:15 -0400

    init

---
+++ Terminal.java
@@ -0,0 +1,20 @@
+package app6;
+
+/** Cette classe identifie les terminaux reconnus et retournes par
+ *  l'analyseur lexical
+ */
+public class Terminal {
+
+
+// Constantes et attributs
+  public String chaine;
+
+
+/** Un ou deux constructeurs (ou plus, si vous voulez)
+  *   pour l'initalisation d'attributs 
+ */	
+  public Terminal(String chaine) {   // arguments possibles
+    this.chaine = chaine;
+  }
+
+}
